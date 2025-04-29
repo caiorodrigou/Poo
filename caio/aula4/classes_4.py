fe3dc3d5c42e3994d6258635baefe41b51504912
caiorodrigou <caiorodrigou@gmail.com>
2025-04-29 18:42:06 +0000

    criar arquivos

---
+++ caio/aula4/classes_4.py
@@ -0,0 +1,8 @@
+# Adicione os arquivos ao staging
+git add .
+
+# Faça o commit com uma mensagem descritiva
+git commit -m "Sua mensagem de commit aqui"
+
+# Envie as alterações para o repositório remoto
+git push
