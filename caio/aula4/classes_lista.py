91739974ff66ae1772ad9475a7153fbb0bc6f5d5
caiorodrigou <caiorodrigou@gmail.com>
2025-05-05 19:53:06 +0000

    t

--- caio/aula4/classes_lista.py
+++ caio/aula4/classes_lista.py
@@ -1,5 +1,30 @@
-class circulo:
+class circulo():
     def __init__(self):
-        self.raio = 0
-    def calc_area():
-        return 3.14 *(sel)
+        self.__raio = 0
+
+
+    def set_raio(self, raio):
+        if raio > 0 :
+            self.__raio = raio
+        else:
+            raise ValueError()
+       
+    def get_raio(self):
+        return self.__raio
+   
+    def calc_area(self):
+        return 3.14 * (self.__raio ** 2)
+   
+    def calc_circunferencia(self):
+        return 2 * 3.14 * self.__raio
+   
+class UI:
+    @staticmethod
+    def main():
+        b = circulo()
+        b.set_raio(2)  # Use the instance 'b' to call the method
+        print(b.calc_area())  # Use the instance 'b' to call the method
+        print(b.calc_circunferencia())  # Use the instance 'b' to call the method
+
+
+UI.main()
