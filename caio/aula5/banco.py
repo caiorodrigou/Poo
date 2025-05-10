67dd8ba06ff2e467a7567c34b0f0f37042d4f7e6
caiorodrigou <caiorodrigou@gmail.com>
2025-05-10 15:16:21 +0000

    teste

--- caio/aula5/banco.py
+++ caio/aula5/banco.py
@@ -1,8 +1,35 @@
 class conta():
-    def __init__(self):
-        self.nome = ""
-        self.num_conta = 0
-        self.saldo = 0
+    def __init__(self, nome="", conta=0, saldo=0):
+        self.__nome = nome
+        self.__num_conta = conta
+        self.__saldo = saldo
+
+    def set__nome(self, nome):
+        if nome == "":
+            self.__nome = nome
+        else:
+            raise ValueError()
+        
+    def set__num_conta(self, conta):
+        if conta > 0:
+            self.__num_conta = conta
+        else:
+            raise ValueError()
+    
+    def set_raio(self, raio):
+        if raio > 0 :
+            self.__raio = raio
+        else: 
+            raise ValueError()
+        
+    def get__nome(self):
+        return self.__nome
+    
+    def get__num_conta(self):
+        return self.__num_conta
+    
+    def get__saldo(self):
+        return self.__saldo
 
     def depositar(self):
         numero = int(input("qual o numero da sua conta? "))
@@ -24,3 +51,20 @@ class conta():
             else:
                 self.saldo = self.saldo - valor_sacado 
                 return print (f"seu saldo agora é {self.saldo}")
+class UI():
+    @staticmethod
+    def main():
+        x = conta()
+        print ("Bem vindo ao banco")
+        x.nome = (input("qual o seu nome? "))
+        x.num_conta = int(input("qual o numero da sua conta? "))
+        x.saldo = float(input("qual o seu saldo? "))
+        comando = 0
+        comando = int(input("digite 1 para depositar ou 2 para sacar : "))
+        if comando == 1:
+            return x.depositar()
+        else:
+            if comando == 2:
+                return x.sacar()
+        
+UI.main()
