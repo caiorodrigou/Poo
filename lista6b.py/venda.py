fb06a8986bdc2f77c047be5cd33771935dd5282d
caiorodrigou <caiorodrigou@gmail.com>
2025-06-06 16:26:03 +0000

    ta indo

--- lista6b.py/venda.py
+++ lista6b.py/venda.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 class Venda:
     def __init__(self,id,data,carrinho,total,id_cliente):
         self.id = id
-        self.data = data
+        self.data = datetime
         self.carrinho = carrinho
         self.total = total
         self.id_cliente = id_cliente
@@ -9,4 +10,43 @@ class Venda:
     def __str__(self):
         return f'id: {self.id}, data: {self.data}, carrinho: {self.carrinho}, total: {self.total}, id_cliente:{self.id_cliente}'
 
-    def get
+    def set_id(self,id):
+        if id <= 0:
+            return ValueError(f'id invalido')
+        self.id = id
+    
+    def set_data(self,data):
+        if data < datetime.now :
+            return ValueError(f'data invalida')
+        self.data = data
+
+    def set_carrinho(self,carrinho):
+        if carrinho != True or False:
+            return ValueError(f'carrinho invalido')
+        self.carrinho = carrinho
+
+    def set_total(self,total):
+        if total < 0 :
+            return ValueError(f'o total não pode ser negativo')
+        self.total = total
+    
+    def set_id_cliente(self,id_cliente):
+        if id_cliente <= 0:
+            return ValueError(f'id invalido')
+        self.id_cliente = id_cliente
+
+    def get_id(self):
+        return self.id
+    
+    def get_data(self):
+        return self.data
+    
+    def get_carrinho(self):
+        return self.carrinho
+    
+    def get_total(self):
+        return self.total
+
+    def get_id_cliente(self):
+        return self.id_cliente
+    
---
+++ lista6b.py/venda_item.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+class Venda:
+    def __init__(self,id,qtd,preco,id_venda,id_produto):
+        self.id = id
+        self.qtd = qtd
+        self.preco = preco
+        self.id_venda = id_venda
+        self.id_produto = id_produto
+    
+    def __str__(self):
+        return f'id: {self.id}, data: {self.data}, carrinho: {self.carrinho}, total: {self.total}, id_cliente:{self.id_cliente}'
+
+    def set_id(self,id):
+        if id <= 0:
+            return ValueError(f'id invalido')
+        self.id = id
+    
+    def set_data(self,data):
+        if data < datetime.now :
+            return ValueError(f'data invalida')
+        self.data = data
+
+    def set_carrinho(self,carrinho):
+        if carrinho != True or False:
+            return ValueError(f'carrinho invalido')
+        self.carrinho = carrinho
+
+    def set_total(self,total):
+        if total < 0 :
+            return ValueError(f'o total não pode ser negativo')
+        self.total = total
+    
+    def set_id_cliente(self,id_cliente):
+        if id_cliente <= 0:
+            return ValueError(f'id invalido')
+        self.id_cliente = id_cliente
+
+    def get_id(self):
+        return self.id
+    
+    def get_data(self):
+        return self.data
+    
+    def get_carrinho(self):
+        return self.carrinho
+    
+    def get_total(self):
+        return self.total
+
+    def get_id_cliente(self):
+        return self.id_cliente
+    
